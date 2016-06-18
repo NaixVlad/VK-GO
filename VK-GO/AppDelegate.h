646aa4b85b4dc7afcaf87d2fc2ff5befb2b3f3cb
NaixVlad <Naixxx@icloud.com>
2016-06-18 15:03:48 +0300

    Initial Commit

---
+++ VK-GO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VK-GO
+//
+//  Created by Vladislav Andreev on 18.06.16.
+//  Copyright © 2016 Vladislav Andreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VK-GO/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VK-GO
+//
+//  Created by Vladislav Andreev on 18.06.16.
+//  Copyright © 2016 Vladislav Andreev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
